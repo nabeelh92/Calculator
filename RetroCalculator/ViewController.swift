9e2a26045fa8b800c91da0449eb503844005c803
nhuq <nhuq@central1.com>
2017-07-20 13:47:53 -0700

    Button presses now play a sound

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -7,18 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var btnSound : AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: path!)
+    
+        do{
+            try btnSound = AVAudioPlayer(contentsOf: soundURL)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+    }
+    
+    @IBAction func numberPressed(sender: UIButton) {
+        
+        // stop if currently playing
+        if btnSound.isPlaying{
+            btnSound.play()
+        }
+        
+        btnSound.play()
+    }
+    
+    func playSound() {
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
